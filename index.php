2aea9312fd5d4088ba5dbc5fb3351cbb70aa1945
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2019-10-24 14:54:19 +0100

    added content.php file & minor change to index.php

--- index.php
+++ index.php
@@ -113,26 +113,8 @@
   <h3 class="d-flex justify-content-center">Lastest blog posts</h3> 
 
   <?php if(have_posts()) : ?>
-        <?php while(have_posts()) : the_post(); ?>
-        <div class="blog-post">
-            <h2 class="blog-post-title">
-               <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-              </a>
-            </h2>
-        <p class="blog-post-meta">
-          <?php the_time('F j, Y g: i a'); ?>
-          by <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')) ?>">
-            <?php the_author(); ?>
-          </a>
-        </p>
-          <?php if(has_post_thumbnail()) : ?>
-            <div class="post-thumb">
-              <?php the_post_thumbnail(); ?>
-            </div>
-          <?php endif; ?>
-        <?php the_excerpt(); ?> <!-- can also use less convenient: the_content()-->
-      </div><!--  blog post  -->
+      <?php while(have_posts()) : the_post(); ?>
+          <?php get_template_part('content'); ?>
       <?php endwhile; ?>
       <?php else : ?>
       <p><?php __('No Posts')?></p>
