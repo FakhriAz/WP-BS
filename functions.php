b06a9f48fb54156204753cd8d4f5941e3c355fc3
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-01-20 14:47:55 +0100

    Added team custom type & team icon

--- functions.php
+++ functions.php
@@ -133,3 +133,36 @@ function WP_BS_sec_linker_txt($wp_customize) {
 }
 add_action('customize_register', 'WP_BS_sec_linker_txt');
 
+/**
+ * Register `team` post type
+ */
+function team_post_type() {
+   
+    // Labels
+     $labels = array(
+         'name' => _x("Team", "post type general name"),
+         'singular_name' => _x("Team", "post type singular name"),
+         'menu_name' => 'Team Profiles',
+         'add_new' => _x("Add New", "team item"),
+         'add_new_item' => __("Add New Profile"),
+         'edit_item' => __("Edit Profile"),
+         'new_item' => __("New Profile"),
+         'view_item' => __("View Profile"),
+         'search_items' => __("Search Profiles"),
+         'not_found' =>  __("No Profiles Found"),
+         'not_found_in_trash' => __("No Profiles Found in Trash"),
+         'parent_item_colon' => ''
+     );
+     
+     // Register post type
+     register_post_type('team' , array(
+         'labels' => $labels,
+         'public' => true,
+         'has_archive' => false,
+         'menu_icon' => get_stylesheet_directory_uri() . '/team-icon.png',
+         'rewrite' => false,
+         'supports' => array('title', 'editor', 'thumbnail')
+     ) );
+ }
+ add_action( 'init', 'team_post_type', 0 );
+
