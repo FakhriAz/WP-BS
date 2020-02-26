5a1b52ee7a162b5167371a757e509d2afa03648f
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-02-26 14:58:48 +0100

    Unnecessary code & comments deleted

--- index.php
+++ index.php
@@ -4,21 +4,18 @@
   <div class="container">
     <!-- <div class="row d-md-block"> -->
       <!-- Sidebar widget -->
-
       <div class="posts-content">
-        <h3>start posts content</h3> 
          <?php if(have_posts()) : ?>
              <?php while(have_posts()) : the_post(); ?>
           <?php get_template_part('content', get_post_format()); ?>
              <?php endwhile; ?>
-             <h3>end posts content</h3>
       <!-- Add pagination -->
             <?php pagination_nav(); ?>
              <?php else : ?>
              <p><?php __('No Posts', 'bs-wp')?></p>
         <?php endif; ?>
       </div><!-- posts div  -->
-
+      
     <div class="sidebar">
       <!-- Top right sidebar widget start -->
       <?php if ( is_active_sidebar( 'info-top-right' ) ) : ?>
@@ -40,9 +37,6 @@
       <?php endif; ?>     
       <!-- Top right sidebar widget end -->  
       </div>   <!--sidebars--> 
-
-        <!-- </div> -->
-    <!-- </div> row -->
   </div> <!-- Container -->
   <!--blog-->
 <?php get_footer(); ?>
